12599e917fff729faf2f9f13a55f0deaa9c1be1a
kasantos2 <kasantos2@up.edu.ph>
2019-09-05 23:43:54 +0800

    updated sablay.py

--- sablay.py
+++ sablay.py
@@ -55,10 +55,10 @@ val_load = torch.utils.data.DataLoader(dataset = dataset,
                                        sampler = SubsetRandomSampler(val_indices))
 
 #Define the model, the loss function and the optimizer
-model = models.resnet152()
+model = models.resnet18()
 loss_fcn = nn.CrossEntropyLoss()
 optimizer = torch.optim.SGD(model.parameters(), lr = 0.01)
-scheduler = torch.optim.lr_scheduler.StepLR(optimizer, step_size=4, gamma=0.1)
+#scheduler = torch.optim.lr_scheduler.StepLR(optimizer, step_size=4, gamma=0.1)
 
 #Define the lists to store the results of loss and accuracy
 train_loss = []
@@ -88,6 +88,7 @@ for epoch in range(num_epochs):
 
         # If we have GPU, shift the data to GPU
         if torch.cuda.is_available():
+            model.cuda()
             inputs = inputs.cuda()
             labels = labels.cuda()
 
