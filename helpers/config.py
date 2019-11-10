471ff6f6be498d75be9c14c74b687b53ffa87691
kasantos2 <kasantos2@up.edu.ph>
2019-11-10 10:29:09 +0800

    updated files

--- helpers/config.py
+++ helpers/config.py
@@ -7,6 +7,7 @@ ORIGINAL_INPUT_TESTING_IMAGES_PATH = "ODIR-5K/ODIR-5K_Testing_Images"
 ORIGINAL_INPUT_LABELS_FILE = "ODIR-5K/ODIR-5K_Training_Annotations(Updated)_V2.csv"
 
 DATASET_CLASSES_PATH = "dataset/classes"
+DATASET_CLASSES_CROPPED_PATH = "dataset/classes_cropped"
 DATASET_DATASET_PATH = "dataset/dataset"
 OUTPUT_PATH = "output"
 
